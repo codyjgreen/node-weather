5de2effaa5f38f8f98befb76a78ef36e5ff6957e
cody <codyjeromegreen@gmail.com>
2018-07-18 12:49:00 -0700

    weather app working its simple but was a fun little exercise

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 require('dotenv').config();
 let apiKey = process.env.API_KEY;
 let city = 'burien';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 const request = require('request');
 request(url, function(err, response, body) {
   if (err) {
     console.log('error: ', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
